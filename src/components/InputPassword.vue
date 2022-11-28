f3633252a5da6dc2f626216f1abd31cb89b8834a
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-11-28 22:10:30 +0100

    Para revisión

--- src/components/InputPassword.vue
+++ src/components/InputPassword.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref } from 'vue';
 
+const mostrar = ref(false)
+const password = ref('Password')
+
 </script>
 
 <template>
     <div class="container text-center input-password">
         <h1 class="my-3">Exercise Input Password</h1>
-        <input type="password" name="" id="password" class="form-control">
+        <!-- <input type="password" name="" id="password" class="form-control"> -->
+        <input :type="mostrar ? 'text' : 'password'" v-model="password" class="form-control">
         <div class="d-block">
-            <span id="show-password" class="mt-2 btn btn-primary">Show Password</span>
+            <span id="show-password" class="mt-2 btn btn-primary" @click="mostrar = !mostrar">Show Password</span>
         </div>
     </div>
 </template>
